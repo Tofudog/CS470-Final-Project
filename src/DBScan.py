97bb1bed01f7d813c4d0d21f1c9ab20757d30f5f
jmsb-code <pjimmythedude@gmail.com>
2025-12-06 22:58:06 -0500

    finished (?) DBScan algo

--- src/DBScan.py
+++ src/DBScan.py
@@ -42,21 +42,20 @@ def DBScan(datafile, eps, minSamp, out):
     df = df.drop(["Latitude", 'Longitude'], axis=1)
 
     #shuffle dataset, to avoid always classify border points the same way
-    df = df.sample(frac=1)
-    df = df.reset_index(drop=False) #reset indices
+    df = df.sample(frac=1).reset_index(drop=True) #reset indices
 
     #define a cluster column, initializing each row as an outlier
     df["Cluster"] = -1
 
     #convert longitude/latitude from degrees to radians
-    locs = np.array(df["Location"])
+    locs = np.array(df["Location"].tolist(), dtype=float)
     locs_rad = np.radians(locs)
 
     #build a BallTree to efficiently find neighbors
     tree = BallTree(locs_rad, metric='haversine')
 
     #convert eps into radians (km = radians * earth-radius)
-    eps_rad = eps / 6,371
+    eps_rad = eps / 6371.0
 
     #get neighbors for each point
     neighbors = tree.query_radius(locs_rad, r=eps_rad)
@@ -66,6 +65,7 @@ def DBScan(datafile, eps, minSamp, out):
 
     #start cluster counter at 1
     curCluster = 1
+    print("Looking for cluster 1...")
 
     for i in visited:
 
@@ -81,12 +81,12 @@ def DBScan(datafile, eps, minSamp, out):
     	if(len(nHood) < minSamp): continue
 
     	#we have a cluster!
-    	df.iloc[i]["Cluster"] = curCluster
+    	df.loc[i, "Cluster"] = curCluster
 
     	neighbor_stack = nHood
 
     	while neighbor_stack:
-    		i = stack.pop()
+    		i = neighbor_stack.pop()
 
     		if not visited[i]:
     			visited[i] = True
@@ -95,15 +95,13 @@ def DBScan(datafile, eps, minSamp, out):
     			if len(nHood) >= minSamp:
     				neighbor_stack.extend(nHood)
 
-    		df.iloc[i]["Cluster"] = curCluster
+    		df.loc[i, "Cluster"] = curCluster
 
     	curCluster += 1
+    	print(f"Cluster {curCluster-1} complete. Looking for cluster {curCluster+1}...")
+
 
     print(f"Algorithm Complete, outputting to {out}")
-
-    #resort as it was before, drop indices, and save
-    df = df.sort_values(by='index')
-    df = df.drop(['index'], axis=1)
 
     df.to_csv(out, index=False)
 
